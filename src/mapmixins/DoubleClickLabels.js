9fcb13ef6917e332aa098b9cf24c6d06a1d2deb0
Joe Moran <joemoran231@gmail.com>
2025-07-10 12:43:28 -0400

    fix: check for null of object before function call

--- src/mapmixins/DoubleClickLabels.js
+++ src/mapmixins/DoubleClickLabels.js
@@ -10,7 +10,9 @@ L.Map.DoubleClickLabels = L.Map.DoubleClickZoom.extend({
   enable() {
     const map = this._map;
 
-    if (this._enabled) { return this; }
+    if (this._enabled) {
+      return this;
+    }
 
     // disable 'doubleClickZoom' if 'doubleClickLabels' is enabled.
     if (map.doubleClickZoom.enabled()) {
@@ -25,7 +27,9 @@ L.Map.DoubleClickLabels = L.Map.DoubleClickZoom.extend({
   },
 
   disable() {
-    if (!this._enabled) { return this; }
+    if (!this._enabled) {
+      return this;
+    }
 
     this._enabled = false;
     this.removeHooks();
@@ -38,16 +42,23 @@ L.Map.DoubleClickLabels = L.Map.DoubleClickZoom.extend({
     const oe = e.originalEvent;
 
     // prevents deselection in case of box selector
-    if (oe && oe.shiftKey) { return; }
+    if (oe && oe.shiftKey) {
+      return;
+    }
 
     map._clicked += 1;
-    this._map._clickTimeout = setTimeout(function() {
+    this._map._clickTimeout = setTimeout(function () {
       if (map._clicked === 1) {
         map._clicked = 0;
         map.fire('singleclick', {type: 'singleclick'});
       } else {
         // manually fire doubleclick event only for touch screens that don't natively fire it
-        if (L.Browser.touch && (oe && oe.sourceCapabilities.firesTouchEvents)) {
+        if (
+          L.Browser.touch &&
+          oe &&
+          oe.sourceCapabilities &&
+          oe.sourceCapabilities.firesTouchEvents
+        ) {
           map.fire('dblclick');
         }
       }
@@ -58,12 +69,14 @@ L.Map.DoubleClickLabels = L.Map.DoubleClickZoom.extend({
     const map = this._map;
     const labels = map._labels;
 
-    setTimeout(function() {
+    setTimeout(function () {
       map._clicked = 0;
       clearTimeout(map._clickTimeout);
     }, 0);
 
-    if (!labels) { return; }
+    if (!labels) {
+      return;
+    }
 
     if (labels.options.opacity === 1) {
       labels.options.opacity = 0;
--- src/mapmixins/DoubleClickZoom.js
+++ src/mapmixins/DoubleClickZoom.js
@@ -5,21 +5,29 @@
  */
 L.Map.DoubleClickZoom.include({
   addHooks() {
-    this._map.on({
-      click: this._fireIfSingle,
-      dblclick: this._onDoubleClick,
-    }, this);
+    this._map.on(
+      {
+        click: this._fireIfSingle,
+        dblclick: this._onDoubleClick,
+      },
+      this
+    );
   },
 
   removeHooks() {
-    this._map.off({
-      click: this._fireIfSingle,
-      dblclick: this._onDoubleClick,
-    }, this);
+    this._map.off(
+      {
+        click: this._fireIfSingle,
+        dblclick: this._onDoubleClick,
+      },
+      this
+    );
   },
 
   enable() {
-    if (this._enabled) { return this; }
+    if (this._enabled) {
+      return this;
+    }
 
     // don't enable 'doubleClickZoom' unless 'doubleClickLabels' is disabled first
     if (this._map.doubleClickLabels) {
@@ -37,7 +45,9 @@ L.Map.DoubleClickZoom.include({
   },
 
   disable() {
-    if (!this._enabled) { return this; }
+    if (!this._enabled) {
+      return this;
+    }
 
     // signify to collection/instance safe to swap 'singleclick' listeners with 'click' listeners.
     this._map.fire('singleclickoff');
@@ -52,16 +62,23 @@ L.Map.DoubleClickZoom.include({
     const oe = e.originalEvent;
 
     // prevents deselection in case of box selector
-    if (oe && oe.shiftKey) { return; }
+    if (oe && oe.shiftKey) {
+      return;
+    }
 
     map._clicked += 1;
-    this._map._clickTimeout = setTimeout(function() {
+    this._map._clickTimeout = setTimeout(function () {
       if (map._clicked === 1) {
         map._clicked = 0;
         map.fire('singleclick', {type: 'singleclick'});
       } else {
         // manually fire doubleclick event only for touch screens that don't natively fire it
-        if (L.Browser.touch && (oe && oe.sourceCapabilities.firesTouchEvents)) {
+        if (
+          L.Browser.touch &&
+          oe &&
+          oe.sourceCapabilities &&
+          oe.sourceCapabilities.firesTouchEvents
+        ) {
           /*  in `DoubleClickLabels.js`, we just do map.fire('dblclick') bc `_onDoublClick` doesn't use the
           passed "e" (for now). To generate a 'real' DOM event that will have all of its corresponding core
           properties (originalEvent, latlng, etc.), use Leaflet's `#map._fireDOMEvent` (Leaflet 1.5.1 source) */
@@ -75,12 +92,14 @@ L.Map.DoubleClickZoom.include({
     const map = this._map;
     const oe = e.originalEvent;
 
-    setTimeout(function() {
+    setTimeout(function () {
       map._clicked = 0;
       clearTimeout(map._clickTimeout);
     }, 0);
 
-    if (!oe) { return false; }
+    if (!oe) {
+      return false;
+    }
 
     const oldZoom = map.getZoom();
     const delta = map.options.zoomDelta;
